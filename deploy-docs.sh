ed83de46f88470c7c50dcbf03f599383a0cdf936
sahachide <sahachide@gmail.com>
2020-09-19 18:29:56 +0200

    chore: setup github-pages

---
+++ deploy-docs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+npm run docs:build
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:sahachide/ZenTS.git master:gh-pages
+cd -
