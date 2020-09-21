e04115e1a6405f691655e0e87fdc6ccc87e5bd4d
sahachide <sahachide@gmail.com>
2020-09-21 17:53:48 +0200

    test: AutoLoader and Registry tests

---
+++ test/common/autoloader_registry.test.ts
@@ -0,0 +1,22 @@
+import { Autoloader } from '../../src'
+import { loadFixtureTestAppConfig } from '../helper/loadFixtureTestAppConfig'
+
+describe('AutoLoader and Registry', () => {
+  beforeAll(async () => {
+    await loadFixtureTestAppConfig()
+    process.env.NODE_ENV = 'development'
+  })
+
+  afterAll(() => {
+    process.env.NODE_ENV = 'test'
+  })
+
+  it('should let the AutoLoader create a Registry', async () => {
+    const autoLoader = new Autoloader()
+    const registry = await autoLoader.createRegistry()
+
+    expect(registry.getControllers().size).toBeGreaterThanOrEqual(3)
+    expect(registry.getServices().size).toBeGreaterThanOrEqual(1)
+    expect(registry.getConnection()).toBeNull()
+  })
+})
