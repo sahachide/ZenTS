835359fd5beb1ca7bdbf452ef36a55f6966b0612
sahachide <sahachide@gmail.com>
2020-09-21 17:27:38 +0200

    test: ControllerFactory and ControllerLoader tests

---
+++ test/controller/controllerFactory.test.ts
@@ -0,0 +1,54 @@
+import { ControllerFactory } from '../../src/controller/ControllerFactory'
+import { ControllerLoader } from '../../src/controller/ControllerLoader'
+import type { Controllers } from '../../src/types'
+import DepdendencyInjectionService from '../fixtures/testapp/src/service/DepdendencyInjectionService'
+import type DependencyInjectionController from '../fixtures/testapp/src/controller/DependencyInjectionController'
+import type ResponseController from '../fixtures/testapp/src/controller/ResponseController'
+import { loadFixtureTestAppConfig } from '../helper/loadFixtureTestAppConfig'
+
+let controllers: Controllers
+
+describe('ControllerFactory', () => {
+  beforeAll(async () => {
+    await loadFixtureTestAppConfig()
+
+    process.env.NODE_ENV = 'development'
+    const controllerLoader = new ControllerLoader()
+    controllers = await controllerLoader.load()
+  })
+
+  afterAll(() => {
+    process.env.NODE_ENV = 'test'
+  })
+
+  it('should build controller', () => {
+    const controllerFactory = new ControllerFactory(controllers, null, {
+      files: [],
+      filters: new Map(),
+    })
+
+    const instance = controllerFactory.build<ResponseController>('responsecontroller')
+
+    expect(instance).not.toBeNull()
+    expect(typeof instance.ping).toBe('function')
+
+    const notExists = controllerFactory.build('loremipsumloremipsumloremipsum')
+
+    expect(notExists).toBeNull()
+  })
+
+  it('should inject dependencies', () => {
+    const controllerFactory = new ControllerFactory(controllers, null, {
+      files: [],
+      filters: new Map(),
+    })
+
+    const instance = controllerFactory.build<DependencyInjectionController>(
+      'dependencyinjectioncontroller',
+    )
+
+    expect(instance).not.toBeNull()
+    expect(instance.injectedService).toBeInstanceOf(DepdendencyInjectionService)
+    expect(instance.valueFromInjectedService()).toBe('foo')
+  })
+})
---
+++ test/fixtures/testapp/src/controller/ResponseFaultController.ts
@@ -0,0 +1,4 @@
+import { Controller, controller } from '../../../../../lib'
+
+@controller('Response')
+export default class extends Controller {}
