726e88877f71fd5944d013c91b5f7959683693b3
sahachide <sahachide@gmail.com>
2020-10-15 17:27:31 +0200

    docs: change search engine to algolia

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -159,6 +159,10 @@ module.exports = {
     lastUpdated: 'Last Updated',
     smoothScroll: false,
     sidebarDepth: 3,
+    algolia: {
+      apiKey: '1c03de24a87faf1bfd8b7e760c63b3ab',
+      indexName: 'zents',
+    },
     nav: [
       {
         text: 'Home',
