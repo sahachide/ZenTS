377eb557c3447a0118395b0cf198bfdf00b25069
sahachide <sahachide@gmail.com>
2020-10-19 14:33:09 +0200

    chore: only load entities when database support is enabled

--- src/database/EntityLoader.ts
+++ src/database/EntityLoader.ts
@@ -1,11 +1,17 @@
 import { AbstractZenFileLoader } from '../filesystem/AbstractZenFileLoader'
 import type { Entities } from '../types/types'
+import { config } from '../config/config'
 import { fs } from '../filesystem/FS'
 import { log } from '../log/logger'
 
 export class EntityLoader extends AbstractZenFileLoader {
   public async load(): Promise<Entities> {
     const entities = new Map() as Entities
+
+    if (!config.database?.enable) {
+      return entities
+    }
+
     const filePaths = await fs.readDir(fs.resolveZenPath('entity'), false)
 
     for (const filePath of filePaths) {
